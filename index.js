02c3e13205733c172f0ff1868052d17200700d0c
Zach <zawood11@gmail.com>
2021-04-07 18:35:42 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(feet){
+  let result;
+  if (feet <= 400) {
+    result = 'This one is on me!';
+  }
+  else if (feet > 2500) {
+    result = 'No can do.';
+  }
+  else if (feet > 2000) {
+    result = 'I will gladly take your thirty bucks.';
+  }
+  return result;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  const result = (city === 'NYC') ? 'Ok, sounds good.' : 'No go.';
+
+  return result;
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let result;
+  if (tip === 'generous') {
+    result = 'Thank you so much.';
+  }
+  else if (tip === 'not as generous') {
+    result = 'Thank you.';
+  }
+  else {
+    result = 'Bye.';
+  }
+
+  return result;
 }
